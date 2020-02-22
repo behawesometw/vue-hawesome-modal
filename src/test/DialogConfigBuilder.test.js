9d9036c9444ce7c728c832f28d14a30d6724a3d8
behawesometw <behawesometw@gmail.com>
2020-02-22 19:32:35 +0800

    build: testing & changelogs

---
+++ src/test/DialogConfigBuilder.test.js
@@ -0,0 +1,32 @@
+import DialogConfigBuilder from "../../lib/dialog/dialogConfigBuilder";
+
+describe("test DialogConfigBuilder", () => {
+    it("ctor call", () => {
+        var builder = new DialogConfigBuilder("test");
+        expect(builder.config.content).toEqual("test");
+    })
+
+    it("defaultConfig", () => {
+        var defaultConfig = DialogConfigBuilder.defaultConfig();
+        expect(typeof defaultConfig).toEqual("object");
+    })
+
+    it("set return builder", () => {
+        var builder = new DialogConfigBuilder("test");
+        var intance = builder.set("isShow", true);
+        expect(intance instanceof DialogConfigBuilder).toBeTruthy();
+    })
+
+    it("set", () => {
+        var builder = new DialogConfigBuilder("test");
+        builder.set("isShow", true);
+        expect(builder.config.isShow).toEqual(true);
+    })
+
+    it("set attr as null value will be ignored", () => {
+        var builder = new DialogConfigBuilder("test");
+        var intance = builder.set("isShow", null);
+        expect(intance instanceof DialogConfigBuilder).toBeTruthy();
+        expect(builder.config.isShow).toEqual(undefined);
+    })
+})
