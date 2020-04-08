165c85f3779576d2f61ca13e41db71aa5cb6b57f
hauer_hsu <hauer_hsu@cht.com.tw>
2020-04-08 14:38:48 +0800

    lib css extract set to false

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
+    css: { extract: false },
     assetsDir: 'assets',
     publicPath: process.env.NODE_ENV === 'production'
         ? '/vue-hawesome-modal/'
-        : '/',
+        : '/'
 };
